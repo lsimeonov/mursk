ee1ccb26e69bd352f8c73b500d33930f5c05ebba
Mariyan Rusev <rusev.mariyan@gmail.com>
2019-03-20 14:59:14 +0200

    hi usak

---
+++ trainingagain2.php
@@ -0,0 +1,24 @@
+<?php
+
+$realname = true;
+$fakename = false;
+
+if ($realname){
+    echo 'mariyan is the name';
+}else{
+    echo 'this name is fake bro';
+}
+
+$sun = true;
+$rain = false;
+
+if ($sun){
+    echo ' it is summer bro';
+}else{
+    echo 'fucken winter bro';
+
+
+$integer = 10;
+$integer2 = 22;
+echo $integer.$integer2 
+}
